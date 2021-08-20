2e5ca8c1aff0d7f111715d3ea638c8fd3965d549
bhsoham <sohambhattacharjee0711@gmail.com>
2021-08-21 02:56:35 +0530

    v1

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 var btnTranslate = document.querySelector("#btn-translate");
-
 var txtInput = document.querySelector("#txt-input");
+var txtOutput = document.querySelector("#txt-output");
 
-btnTranslate.addEventListener("click",function clickEventHandler(){
-    console.log("button clicked!");
-    console.log(txtInput.value);
-});
+// var bananaUrl="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+var bananaUrl = "https://api.funtranslations.com/translate/minion.json";
 
+function constructUrl(text){
+    return bananaUrl+"?"+"text="+text;
+} 
 
+function errorHandler(error){
+    alert("error occured: "+ error)
+}
 
+function clickEventHandler() {
+    var txtToTranslate = txtInput.value;
+
+    fetch(constructUrl(txtToTranslate))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            txtOutput.innerText = translatedText;
+        })
+        .catch(errorHandler)
+};
+
+btnTranslate.addEventListener("click",clickEventHandler)
